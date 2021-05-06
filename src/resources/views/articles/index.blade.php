e658269a9a2ea6aa5bd44c426be8f1771263c495
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-05-06 11:46:35 +0900

    bootstrap navbar

--- src/resources/views/articles/index.blade.php
+++ src/resources/views/articles/index.blade.php
@@ -3,14 +3,15 @@
 @section('title', '記事一覧')
 
 @section('content')
+    @include('nav')
     <div class="container">
         @foreach($articles as $article)
-            <div>
-                {{ $article->title }}
-            </div>
-            <div>
-                {{ $article->body }}
-            </div>
+        <div>
+            {{ $article->title }}
+        </div>
+        <div>
+            {{ $article->body }}
+        </div>
         @endforeach
     </div>
 @endsection
