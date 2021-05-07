035b43290676d284dfb81170529e08cd48a10570
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-05-07 11:22:39 +0900

    fix route_controller

--- src/app/Http/Controllers/ArticleController.php
+++ src/app/Http/Controllers/ArticleController.php
@@ -9,19 +9,6 @@ class ArticleController extends Controller
 {
     public function index()
     {
-        /*
-        $articles = [
-            (object) [
-                'title' => 'タイトル1',
-                'body' => '本文1'
-            ],
-            (object) [
-                'title' => 'タイトル2',
-                'body' => '本文2'
-            ]
-        ];
-        */
-
         $articles = Article::all()->sortByDesc('created_at');
 
         return view('articles.index', ['articles' => $articles]);
