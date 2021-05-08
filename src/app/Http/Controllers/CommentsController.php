679bbf7d8f1bca1c5e2858d2f8414a41cbbc3e06
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-05-08 11:34:02 +0900

    comments_controller add_action

--- src/app/Http/Controllers/CommentsController.php
+++ src/app/Http/Controllers/CommentsController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
-    //
+    public function create()
+    {
+        //
+    }
+
+    public function edit()
+    {
+        //
+    }
+
+    public function update()
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
 }
