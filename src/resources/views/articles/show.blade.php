2f622560f173d05d4e3cd2d3c2b0a459b117a4fb
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-05-09 18:00:42 +0900

    view show comment

--- src/resources/views/articles/show.blade.php
+++ src/resources/views/articles/show.blade.php
@@ -3,15 +3,24 @@
 @section('title', '記事詳細')
 
 @section('content')
-@include('nav')
-<div class="container">
-    @include('articles.card')
-</div>
-<div class="mb-3">
-    <label for="exampleFormControlTextarea1" class="form-label">コメント入力</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div>
-<form method="POST" action="{{ route('comments.store') }}">
-    <button type="submit" class="btn blue-gradient btn-block">投稿する</button>
-</form>
+    @include('nav')
+    <div class="container">
+        @include('articles.card')
+    </div>
+    <div class=h4>コメント</div>
+    <div>
+        @foreach($comments as $comment)
+        <div class="font-weight-bold">{{ $comment->user->name }}</div>
+        <div class="font-weight-lighter">{{ $comment->created_at->format('Y/m/d H:i') }}</div>
+        <div>{{ $comment->body }}</div>
+        @endforeach
+    </div>
+
+    <form method="POST" action="{{ route('comments.store') }}">
+        @csrf
+        <div class="form-group">
+            <textarea name="body" class="form-control" rows="16" placeholder="コメントを入力してください">{{ $comment->body ?? old('body') }}</textarea>
+        </div>
+        <button type="submit" class="btn blue-gradient btn-block">投稿する</button>
+    </form>
 @endsection
