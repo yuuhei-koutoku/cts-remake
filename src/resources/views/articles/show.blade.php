0b89c42fda8c3c816ecdc8381f61b7ca4022dd7f
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-05-09 09:04:18 +0900

    view show comment tmp

--- src/resources/views/articles/show.blade.php
+++ src/resources/views/articles/show.blade.php
@@ -3,8 +3,15 @@
 @section('title', '記事詳細')
 
 @section('content')
-    @include('nav')
-    <div class="container">
-        @include('articles.card')
-    </div>
+@include('nav')
+<div class="container">
+    @include('articles.card')
+</div>
+<div class="mb-3">
+    <label for="exampleFormControlTextarea1" class="form-label">コメント入力</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+<form method="POST" action="{{ route('comments.store') }}">
+    <button type="submit" class="btn blue-gradient btn-block">投稿する</button>
+</form>
 @endsection
