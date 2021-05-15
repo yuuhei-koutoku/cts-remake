f19606c32024e369ff69648f77fb8f642b341f55
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-05-15 18:27:32 +0900

    fix controller serch

--- src/app/Http/Controllers/ArticleController.php
+++ src/app/Http/Controllers/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Article;
 use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ArticleController extends Controller
 {
@@ -16,7 +17,25 @@ class ArticleController extends Controller
     public function index(Request $request)
     {
         $search = $request->input('search');
-        dd($request);
+        /*
+        $query = DB::table('articles');
+        if($search !== null){
+            //全角スペースを半角に
+            $search_split = mb_convert_kana($search,'s');
+
+            //空白で区切る
+            $search_split2 = preg_split('/[\s]+/', $search_split,-1,PREG_SPLIT_NO_EMPTY);
+
+            //単語をループで回す
+            foreach($search_split2 as $value)
+            {
+                $query->where('body','like','%'.$value.'%');
+            }
+        }
+        $query->select('id', 'title', 'body', 'created_at');
+        $query->orderBy('created_at', 'asc');
+        $articles = $query;
+        */
 
         $articles = Article::all()->sortByDesc('created_at');
 
