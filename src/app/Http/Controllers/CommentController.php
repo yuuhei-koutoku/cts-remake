be4ad68d1215c75cb3a213e800f33a037ae78a69
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-05-22 12:59:12 +0900

    comment_dev thinking

--- src/app/Http/Controllers/CommentController.php
+++ src/app/Http/Controllers/CommentController.php
@@ -10,32 +10,12 @@ class CommentController extends Controller
 {
     public function store(CommentRequest $request, Comment $comment)
     {
-        /*
-        $user = auth()->user();
-        $comment->fill($request->all());
-        $comment->user_id = $request->user()->id;
-        //$comment->user_id = $user->id;
-        $comment->save();
-        return redirect()->route('articles.show');
-        */
-
-        $comment->fill($request->all());
-        $comment = new Comment();
-        $comment->body = $request->body;
-        $comment->save();
-        return redirect()->route('articles.show');
-    }
-
-    /*
-    public function store(CommentRequest $request, Comment $comment)
-    {
         $comment->fill($request->all());
         $comment->user_id = $request->user()->id;
         $comment->article_id = $request->article()->id;
         $comment->save();
         return redirect()->route('articles.show');
     }
-    */
 
     public function edit()
     {
