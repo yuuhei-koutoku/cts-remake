8729297a237a0ed83669d105abd0f707b7483940
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-05-22 15:24:38 +0900

    tag migrate

---
+++ src/database/migrations/2021_05_22_053342_create_article_tag_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticleTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_tag', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('article_id')
+                ->unsigned()
+                ->onDelete('cascade');
+            $table->bigInteger('tag_id')
+                ->unsigned()
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article_tag');
+    }
+}
