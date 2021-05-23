9b1130bd1adb63f4cdb5572d0c708040bc99d37e
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-05-23 13:33:45 +0900

    fix show simple

--- src/resources/views/articles/show.blade.php
+++ src/resources/views/articles/show.blade.php
@@ -1,21 +1,11 @@
 @extends('app')
 
-@section('title', $tag->hashtag)
+@section('title', '詳細ページ')
 
 @section('content')
     @include('nav')
     <div class="container">
-        <div class="card mt-3">
-            <div class="card-body">
-                <h2 class="h4 card-title m-0">{{ $tag->hashtag }}</h2>
-                <div class="card-text text-right">
-                    {{ $tag->articles->count() }}件
-                </div>
-            </div>
-        </div>
-        @foreach($tag->articles as $article)
-            @include('articles.card')
-        @endforeach
+        @include('articles.card')
     </div>
 
     @auth
