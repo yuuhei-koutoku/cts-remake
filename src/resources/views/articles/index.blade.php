1365afcbb31321f160a5e8c5c5444feb704f9b3f
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-05-30 07:10:35 +0900

    serch_box margin_padding

--- src/resources/views/articles/index.blade.php
+++ src/resources/views/articles/index.blade.php
@@ -8,8 +8,8 @@
     <div class="row">
         <div class="col-md-4">
             <form method="GET" action="{{ route('articles.index') }}" class="d-flex">
-                <input class="form-control me-2" name="search" type="search" placeholder="検索" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">検索する</button>
+                <input class="form-control me-2 mt-3" name="search" type="search" placeholder="検索" aria-label="Search">
+                <button class="btn btn-outline-success mt-3 py-0" type="submit">検索する</button>
             </form>
         </div>
         @foreach($articles as $article)
