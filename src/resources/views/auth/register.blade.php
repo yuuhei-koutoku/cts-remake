a33bc9f873294d0836dba089bea99d7977985936
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-05-30 07:34:53 +0900

    delete memo

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -7,7 +7,6 @@
 <div class="container">
     <div class="row">
         <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
-            <h1 class="text-center"><a class="text-dark" href="/">memo</a></h1>
             <div class="card mt-3">
                 <div class="card-body text-center">
                     <h2 class="h3 card-title text-center mt-2">ユーザー登録</h2>
@@ -29,6 +28,7 @@
                             <div class="md-form">
                                 <label for="password">パスワード</label>
                                 <input class="form-control" type="password" id="password" name="password" required>
+                                <small>(半角英字（小文字）、半角英字（大文字）、半角数字を少なくとも１文字以上含む8文字以上)</small>
                             </div>
                             <div class="md-form">
                                 <label for="password_confirmation">パスワード(確認)</label>
