712c91f506e16367e0165ed2637fcf0bbc735c2c
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-05-29 15:13:49 +0900

    light_fix article_tag

--- src/resources/views/articles/card.blade.php
+++ src/resources/views/articles/card.blade.php
@@ -64,16 +64,16 @@
     </div>
 
     @foreach($article->tags as $tag)
-    @if($loop->first)
-    <div class="card-body pt-0 pb-4 pl-3">
-        <div class="card-text line-height">
-            @endif
-            <a href="{{ route('tags.show', ['name' => $tag->name]) }}" class="border p-1 mr-1 mt-1 text-muted">
-                {{ $tag->name }}
-            </a>
-            @if($loop->last)
-        </div>
-    </div>
-    @endif
+        @if($loop->first)
+            <div class="card-body pt-0 pb-4 pl-3">
+                <div class="card-text line-height">
+        @endif
+                    <a href="{{ route('tags.show', ['name' => $tag->name]) }}" class="border p-1 mr-1 mt-1 text-muted">
+                        {{ $tag->name }}
+                    </a>
+        @if($loop->last)
+                </div>
+            </div>
+        @endif
     @endforeach
 </div>
