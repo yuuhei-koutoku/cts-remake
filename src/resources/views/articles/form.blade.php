63092ddb5037635b6554b1164a494bf7faeba227
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-05-30 09:43:23 +0900

    from image

--- src/resources/views/articles/form.blade.php
+++ src/resources/views/articles/form.blade.php
@@ -14,3 +14,7 @@
     <label></label>
     <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
 </div>
+<input id="image" type="file" name="image">
+<button type="submit">
+    アップロード
+</button>
