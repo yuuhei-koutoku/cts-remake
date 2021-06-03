7d94e1d40f6872559c0333971656de2606604967
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-06-03 22:03:08 +0900

    view fix_form

--- src/resources/views/articles/form.blade.php
+++ src/resources/views/articles/form.blade.php
@@ -1,19 +1,22 @@
 @csrf
-<div class="md-form">
-    <label>タイトル</label>
-    <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
-</div>
-<div class="form-group">
-    <article-tags-input
-    :initial-tags='@json($tagNames ?? [])'
-    :autocomplete-items='@json($allTagNames ?? [])'
-    >
-    </article-tags-input>
-</div>
-<div class="form-group">
-    <label></label>
-    <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
-</div>
-<div class="form-group">
-    <input type="file" class="from-control-file" id="image" name="image">
+<div class="container">
+    <div class="row">
+        <div class="offset-md-2 col-md-8">
+            <div class="md-form">
+                <label>タイトル</label>
+                <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
+            </div>
+            <div class="form-group">
+                <article-tags-input :initial-tags='@json($tagNames ?? [])' :autocomplete-items='@json($allTagNames ?? [])'>
+                </article-tags-input>
+            </div>
+            <div class="form-group">
+                <label></label>
+                <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
+            </div>
+            <div class="form-group">
+                <input type="file" class="from-control-file" id="image" name="image">
+            </div>
+        </div>
+    </div>
 </div>
