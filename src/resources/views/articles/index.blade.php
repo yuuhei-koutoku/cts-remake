1330c775806048b2ddcf8733721589848629510c
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-06-21 22:34:09 +0900

    ariticle_card_design

--- src/resources/views/articles/index.blade.php
+++ src/resources/views/articles/index.blade.php
@@ -6,19 +6,19 @@
 @include('nav')
 <div class="container">
     <div class="row">
-        <div class="col-md-12">
             @guest
             <img src="/images/Construction-pana_r1.png" width="100%">
             @endguest
-            <form method="GET" action="{{ route('articles.index') }}" class="d-flex">
-                <input class="form-control me-2 mt-3" name="search" value="{{ request('search') }}" type="search" placeholder="サイト内検索" aria-label="Search">
-                <button class="btn btn-outline-success mt-3 mb-0 ml-0 py-0" type="submit"><i class="fas fa-search"></i></button>
-            </form>
+            <div class="col-12">
+                <form method="GET" action="{{ route('articles.index') }}" class="d-flex">
+                    <input class="form-control me-2 mt-3" name="search" value="{{ request('search') }}" type="search" placeholder="サイト内検索" aria-label="Search">
+                    <button class="btn btn-outline-success mt-3 mb-0 ml-0 py-0" type="submit"><i class="fas fa-search"></i></button>
+                </form>
+            </div>
 
             @foreach($articles as $article)
             @include('articles.card')
             @endforeach
-        </div>
         {{ $articles->links() }}
     </div>
 </div>
