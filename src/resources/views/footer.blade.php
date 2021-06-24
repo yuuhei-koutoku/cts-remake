7ded4c3c0151ec40cee2f8075300b7ae064e58fc
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-06-25 06:32:01 +0900

    footer_start

---
+++ src/resources/views/footer.blade.php
@@ -0,0 +1,21 @@
+<footer>
+    <div class="logo">
+        <i class="fas fa-building"></i>
+        <span class="logo_style">Construction Technologies</span>
+    </div>
+    <nav class="footer__nav">
+        <ul>
+            @guest
+            <li><a href="{{ route('register') }}">ユーザー登録</a></li>
+            <li><a href="{{ route('login') }}">ログイン</a></li>
+            <li><a href="{{ route('login.guest') }}">ゲストログイン</a></li>
+            @endguest
+            @auth
+            <li><a href="{{ route('articles.create') }}">投稿する</a></li>
+            <li><a href="{{ route('logout') }}">ログアウト</a></li>
+            @endauth
+        </ul>
+    </nav>
+
+
+</footer>
