89d8ba4ac79c412819711c22b963e35bc09a3dcb
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-06-27 07:59:11 +0900

    article_show_outline

--- src/resources/views/articles/show.blade.php
+++ src/resources/views/articles/show.blade.php
@@ -5,7 +5,91 @@
 @section('content')
 @include('nav')
 <div class="container mb-5">
-    @include('articles.card')
+    <div>
+        <div>
+
+            <h3 class="card-title">
+                <a class="article-title" href="{{ route('articles.show', ['article' => $article]) }}">
+                    {{ $article->title }}
+                </a>
+            </h3>
+
+            @foreach($article->tags as $tag)
+            @if($loop->first)
+            <div class="pt-0 pb-3">
+                <div class="card-text line-height">
+                    @endif
+                    <a href="{{ route('tags.show', ['name' => $tag->name]) }}" class="border p-1 mr-1 mt-1 text-muted">
+                        {{ $tag->name }}
+                    </a>
+                    @if($loop->last)
+                </div>
+            </div>
+            @endif
+            @endforeach
+
+            <div>
+                <span class="ligth-font pr-2">{{ $article->user->name }}</span>
+                <span class="ligth-font pr-2">{{ $article->created_at->format('Y/m/d H:i') }}</span>
+                <div class="auth-dropup">
+                    @if( Auth::id() === $article->user_id )
+                    <!-- dropdown -->
+                    <div class="ml-auto card-text">
+                        <div class="dropup">
+                            <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-ellipsis-h"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right">
+                                <a class="dropdown-item" href="{{ route("articles.edit", ['article' => $article]) }}">
+                                    <i class="fas fa-pen mr-1"></i>記事を更新する
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $article->id }}">
+                                    <i class="fas fa-trash-alt mr-1"></i>記事を削除する
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- dropdown -->
+                    <!-- modal -->
+                    <div id="modal-delete-{{ $article->id }}" class="modal fade" tabindex="-1" role="dialog">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <form method="POST" action="{{ route('articles.destroy', ['article' => $article]) }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <div class="modal-body text-center">
+                                        記事を削除します。よろしいですか？
+                                    </div>
+                                    <div class="modal-footer justify-content-between">
+                                        <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
+                                        <button type="submit" class="btn btn-danger">削除する</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- modal -->
+                    @endif
+                </div>
+            </div>
+
+            <div class="article-show-img">
+                @if ($article->image)
+                <img src="{{ $article->image }}" style="width:100%;">
+                @endif
+            </div>
+
+            <div class="text-dark pb-3">
+                {{ $article->body }}
+            </div>
+        </div>
+    </div>
 </div>
 
 <div class="container">
