213170a3baf8e2d5db3fa314ec6a541051d762b1
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-06-26 11:31:20 +0900

    register_login position

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'ユーザー登録')
 
 @section('content')
-<div class="container">
+<div class="container my-5">
     <div class="row">
         <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
             <h1 class="text-center">
