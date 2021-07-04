8ad190a01f412e7f7e3bc2aed80d78a56338e85e
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-07-04 13:10:32 +0900

    fix_article_form create_edit

--- src/resources/views/articles/form.blade.php
+++ src/resources/views/articles/form.blade.php
@@ -1,20 +1,20 @@
 @csrf
-<div class="container">
+<div class="container my-5">
     <div class="row">
         <div class="offset-md-2 col-md-8">
-            <div class="md-form">
-                <label>タイトル</label>
-                <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
+            <div class="mb-3">
+                <label></label>
+                <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}" placeholder="タイトルを入力してください。">
             </div>
-            <div class="form-group">
+            <div class="mb-3">
                 <article-tags-input :initial-tags='@json($tagNames ?? [])' :autocomplete-items='@json($allTagNames ?? [])'>
                 </article-tags-input>
             </div>
-            <div class="form-group">
+            <div class="mb-3">
                 <label></label>
                 <textarea name="body" required class="form-control" rows="16" placeholder="本文を入力してください。">{{ $article->body ?? old('body') }}</textarea>
             </div>
-            <div class="form-group">
+            <div class="mb-3">
                 <input type="file" class="from-control-file" id="image" name="image">
             </div>
         </div>
