93c5ce3752296127f746005bc8f29fb088b3aa27
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-07-04 10:48:03 +0900

    article_form fix_placeholder

--- src/resources/views/articles/form.blade.php
+++ src/resources/views/articles/form.blade.php
@@ -12,7 +12,7 @@
             </div>
             <div class="form-group">
                 <label></label>
-                <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
+                <textarea name="body" required class="form-control" rows="16" placeholder="本文を入力してください。">{{ $article->body ?? old('body') }}</textarea>
             </div>
             <div class="form-group">
                 <input type="file" class="from-control-file" id="image" name="image">
