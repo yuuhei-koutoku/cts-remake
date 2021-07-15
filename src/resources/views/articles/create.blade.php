b919523c3c62c4c222ff8bac5281425cd1564e05
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-07-15 21:48:26 +0900

    arrangement new_line

--- src/resources/views/articles/create.blade.php
+++ src/resources/views/articles/create.blade.php
@@ -5,23 +5,21 @@
 @include('nav')
 
 @section('content')
-    <form method="POST" action="{{ route('articles.store') }}" enctype="multipart/form-data">
-        @include('articles.form')
-
-        <div class="container">
-            <div class="row">
-                <div class="offset-md-2 col-md-8">
-                    <div>
-                        <button type="submit" class="btn aqua-gradient btn-block"><i class="fas fa-pen mr-1"></i>投稿する</button>
-                    </div>
-
-                    <button type="button" class="btn btn-outline-info waves-effect my-5 ml-0">
-                        <a href="/" class="text-info">
-                            <i class="fas fa-angle-double-left"></i> 投稿一覧に戻る
-                        </a>
-                    </button>
+<form method="POST" action="{{ route('articles.store') }}" enctype="multipart/form-data">
+    @include('articles.form')
+    <div class="container">
+        <div class="row">
+            <div class="offset-md-2 col-md-8">
+                <div>
+                    <button type="submit" class="btn aqua-gradient btn-block"><i class="fas fa-pen mr-1"></i>投稿する</button>
                 </div>
+                <button type="button" class="btn btn-outline-info waves-effect my-5 ml-0">
+                    <a href="/" class="text-info">
+                        <i class="fas fa-angle-double-left"></i> 投稿一覧に戻る
+                    </a>
+                </button>
             </div>
         </div>
-    </form>
+    </div>
+</form>
 @endsection
