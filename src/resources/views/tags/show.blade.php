214421c4b6c012459e3aef45b8d4ffad4b209d2c
yuuhei-koutoku <yhkt.0627@gmail.com>
2021-07-25 08:41:26 +0900

    view_tags_commentout

--- src/resources/views/tags/show.blade.php
+++ src/resources/views/tags/show.blade.php
@@ -7,9 +7,11 @@
 <div class="container">
     <div class="card mt-3">
         <div class="card-body">
+            <!-- 選択されたタグ名、タグの件数 -->
             <h2 class="h4 card-title m-0"><strong>{{ $tag->name }}</strong> に関する記事が{{ $tag->articles->count() }}件あります。</h2>
         </div>
     </div>
+    <!-- 選択されたタグが付けられた記事一覧 -->
     @foreach($tag->articles as $article)
     @include('articles.list')
     @endforeach
