39e611396da74364d59493514ffaeedb3c1398f4
yuuhei-koutoku <yhkt.0627@gmail.com>
2022-02-03 22:07:07 +0900

    css_heroku_2

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -10,6 +10,8 @@
     </title>
     @if(config('app.env') === 'production')
         <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
+    @elseif(config('app.env') === 'heroku')
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     @else
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @endif
