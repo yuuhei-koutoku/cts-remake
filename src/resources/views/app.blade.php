cbd27e7c5175cf8181e98151aef331e39b176212
yuuhei-koutoku <yhkt.0627@gmail.com>
2022-02-20 16:29:35 +0900

    test css check 3

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -9,9 +9,9 @@
         @yield('title')
     </title>
     @if(config('app.env') === 'production')
-        <link rel="stylesheet" href="{{ secure_asset('css/test.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
     @else
-        <link rel="stylesheet" href="{{ asset('css/all.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @endif
 
     <!-- Font Awesome -->
